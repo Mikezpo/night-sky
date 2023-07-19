0c7320d766cc6a5775e439dd0c40d578b4ff0972
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-07-19 22:19:57 +0100

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -5,5 +5,19 @@ function getFetch(){
   const choice = document.querySelector('input').value
   const url = `https://api.nasa.gov/planetary/apod?api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M&date=${choice}`
 
-  
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if(data.media_type === 'image') {
+            document.querySelector('img').src = data.hdurl
+        }else if(data.media_type === 'video') {
+            document.querySelector('iframe').src = data.url
+        }
+        document.querySelector('h3').innerHTML = data.explanation
+
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 }

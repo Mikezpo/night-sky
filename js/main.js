f3d28d1341e8c8b0bc41c1e154b97dec76fbbe93
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-08-11 16:24:40 +0100

    Updated

--- js/main.js
+++ js/main.js
@@ -24,20 +24,33 @@ function getFetch(){
 
 // API fetch for Mars view
 
-document.querySelector('#marsImage').addEventListener('click', getMars)
-
-function getMars() {
-    const url = ``;
-
-    fetch(url)
+fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=100&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            document.querySelector('#marsPhoto').src = data.photos[1].img_src
 
-            document.querySelector('.marsPhotoDescr').innerHTML = data.explanation
+            document.querySelector('.marsPhotoDescr').innerHTML = data.photos[22].camera.full_name
         })
         .catch(err => {
             console.log(`error ${err}`)
-        })
-}
+        });
+// document.querySelector('#marsImage').addEventListener('click', getMars)
+
+// function getMars() {
+//     const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=100&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`;
+
+//     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=100&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data)
+//             if(data.media_type === 'image') {
+//                 document.querySelector('img').src = data.hdurl
+
+//             document.querySelector('.marsPhotoDescr').innerHTML = data.explanation
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         })
+// }
 

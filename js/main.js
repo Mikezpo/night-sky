0220ddc2fa92e8251265ff82cde9b37827703ff9
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-08-12 16:55:40 +0100

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -23,44 +23,83 @@ function getFetch(){
 }
 
 // API fetch for Mars view
-let sol = ''
+// let sol = ''
 
-for(let i = 1; i < sol.length -1; i++) {
-    sol = Math.random(sol[i] * 1000);
-}
+// for(let i = 1; i < sol.length -1; i++) {
+//     sol = Math.random(sol[i] * 1000);
+// }
 
-fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=999&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            document.querySelector('#marsPhoto').src = data.photos[0].img_src
-            document.querySelector('.marsCameraType').innerHTML = data.photos[0].camera.full_name
-            document.querySelector('.photoDay').innerHTML = data.photos[0].earth_date
-            document.querySelector('.marsRoverName').innerHTML = data.photos[0].rover.name
-            document.querySelector('.roverTotalDays').innerHTML = data.photos[0].rover.max_sol
+// fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=999&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data)
+//             document.querySelector('#marsPhoto').src = data.photos[0].img_src
+//             document.querySelector('.marsCameraType').innerHTML = data.photos[0].camera.full_name
+//             document.querySelector('.photoDay').innerHTML = data.photos[0].earth_date
+//             document.querySelector('.marsRoverName').innerHTML = data.photos[0].rover.name
+//             document.querySelector('.roverTotalDays').innerHTML = data.photos[0].rover.max_sol
 
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         });
 
+// PREVIOUS CODE
 
 // document.querySelector('#marsImage').addEventListener('click', getMars)
 
 // function getMars() {
-//     const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=100&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`;
 
-//     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=100&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`)
+//     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=999&camera=pancam&api_key=aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M`)
 //         .then(res => res.json())
 //         .then(data => {
 //             console.log(data)
-//             if(data.media_type === 'image') {
-//                 document.querySelector('img').src = data.hdurl
+//             document.querySelector('#marsPhoto').src = data.photos[0].img_src
+//             document.querySelector('.marsCameraType').innerHTML = data.photos[0].camera.full_name
+//             document.querySelector('.photoDay').innerHTML = data.photos[0].earth_date
+//             document.querySelector('.marsRoverName').innerHTML = data.photos[0].rover.name
+//             document.querySelector('.roverTotalDays').innerHTML = data.photos[0].rover.max_sol
 
-//             document.querySelector('.marsPhotoDescr').innerHTML = data.explanation
 //         })
 //         .catch(err => {
 //             console.log(`error ${err}`)
-//         })
+//         });
 // }
+
+// NEW CODE
+
+document.querySelector('#marsImage').addEventListener('click', getMars)
+
+function getMars() {
+    const roverName = 'opportunity';  // You can change this to 'curiosity' or 'perseverance' as needed
+    const cameraType = 'pancam';
+    const apiKey = 'aSLcYwUz5fP5sVIFDISMxQesnaakfPORu74cOw3M';
+    const sol = Math.random(1) * 998 + 1;
+
+    // Clear the error message
+    document.querySelector('.errorImage').innerHTML = '';
+
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${sol}&camera=${cameraType}&api_key=${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            if (data.photos.length > 0) {
+                const photoData = data.photos[0];
+
+                document.querySelector('#marsPhoto').src = photoData.img_src;
+                document.querySelector('.marsCameraType').innerHTML = photoData.camera.full_name;
+                document.querySelector('.photoDay').innerHTML = photoData.earth_date;
+                document.querySelector('.marsRoverName').innerHTML = photoData.rover.name;
+                document.querySelector('.roverTotalDays').innerHTML = photoData.rover.max_sol;
+            } else {
+                console.log('No photos found for the specified rover and camera type.');
+                document.querySelector('.errorImage').innerHTML = 'No photos found for the specified rover and camera type.';
+            }
+        })
+        .catch(err => {
+            console.log(`error ${err}`);
+            
+        });
+}
 
